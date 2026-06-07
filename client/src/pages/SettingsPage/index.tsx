c2699128f42f9b294a0c1343ba15c70860f6d53a
eilinseth <aksun989@gmail.com>
2026-06-08 06:49:49 +0700

    Completing settings feature

--- client/src/pages/SettingsPage/index.tsx
+++ client/src/pages/SettingsPage/index.tsx
@@ -1,5 +1,8 @@
 import {User} from "lucide-react"
 import {ChevronRight} from "lucide-react"
+import {Lock} from "lucide-react"
+import {AlertCircle} from "lucide-react"
+import {LogOut} from "lucide-react"
 
 const Settings = () =>{
     return (
@@ -15,21 +18,63 @@ const Settings = () =>{
                     </div>
                 </div>
             </div>
-            <div className="mt-5 flex flex-col gap-2">
-            <div className="text-lg font-semibold">Account</div>
-            <div className="flex bg-slate-700 gap-2 w-full px-2 py-2 border-b-slate-300">
-                <div className="flex items-center justify-center border-2 w-14 h-10 border-slate-500">
-                    <User className={"stroke-[#E5E7EB] size-5"}/>
+            <div className="mt-5 flex flex-col  ">
+                <div className="text-lg font-semibold mb-1">Account</div>
+                <div className="flex bg-slate-700 gap-2 w-full items-center px-4 py-2 border-b-slate-500 border-slate-400 border cursor-pointer">
+                    <div className="flex items-center justify-center border-2 w-22 h-10 border-slate-500">
+                        <User className={"stroke-[#E5E7EB] size-5"}/>
+                    </div>
+                    <div className="text-base font-semibold text-white flex  px-2 cursor-pointer flex-col w-80">
+                        <div className="font-semibold">Username</div>
+                        <div className="text-sm text-[#9CA3AF] ">admin</div>
+                    </div>
+                    <div className="flex items-center justify-end w-full pr-2">
+                        <ChevronRight className="size-5 cursor-pointer"/>
+                    </div>
                 </div>
-                <div className="   text-sm font-semibold text-white flex items-center px-2 cursor-pointer flex-col">
-                    <div className="font-semibold">Username</div>
-                    <div className="text-xs text-[#9CA3AF]">admin</div>
+                <div className="flex bg-slate-700 gap-2 w-full items-center px-4 py-2 border-b-slate-300  border-slate-400 border cursor-pointer">
+                    <div className="flex items-center justify-center border-2 w-22 h-10 border-slate-500">
+                        <Lock className={"stroke-[#E5E7EB] size-5"}/>
+                    </div>
+                    <div className="text-base font-semibold text-white flex  px-2 cursor-pointer flex-col w-80">
+                        <div className="font-semibold">Password</div>
+                        <div className="text-sm text-[#9CA3AF] ">update password</div>
+                    </div>
+                    <div className="flex items-center justify-end w-full pr-2">
+                        <ChevronRight className="size-5 cursor-pointer"/>
+                    </div>
                 </div>
-                <div className="flex items-center justify-end w-full pr-2">
-                    <ChevronRight className="size-5 cursor-pointer"/>
+            </div> 
+            <div className="mt-5 flex flex-col">
+                <div className="text-lg font-semibold mb-1">App</div>
+                <div className="flex bg-slate-700 gap-2 w-full items-center px-4 py-2 border-slate-400 border cursor-pointer">
+                    <div className="flex items-center justify-center border-2 w-22 h-10 border-slate-500">
+                        <AlertCircle className={"stroke-[#E5E7EB] size-5"}/>
+                    </div>
+                    <div className="text-base font-semibold text-white flex  px-2 cursor-pointer flex-col w-80">
+                        <div className="font-semibold">About</div>
+                        <div className="text-sm text-[#9CA3AF] ">Dashboard APP V.1.0.0</div>
+                    </div>
+                    <div className="flex items-center justify-end w-full pr-2">
+                        <ChevronRight className="size-5 cursor-pointer"/>
+                    </div>
                 </div>
             </div>
-            </div> 
+            <div className="mt-5 flex flex-col">
+                <div className="text-lg font-semibold mb-1">Session</div>
+                <div className="flex bg-slate-700 gap-2 w-full items-center px-4 py-2 border-slate-400 border cursor-pointer">
+                    <div className="flex items-center justify-center border-2 w-22 h-10 border-slate-500">
+                        <LogOut className={"stroke-[#E5E7EB] size-5"}/>
+                    </div>
+                    <div className="text-base font-semibold text-white flex  px-2 cursor-pointer flex-col w-80">
+                        <div className="font-semibold">Logout</div>
+                    </div>
+                    <div className="flex items-center justify-end w-full pr-2">
+                        <ChevronRight className="size-5 cursor-pointer"/>
+                    </div>
+                </div>
+
+            </div>
 
         </section>
     )
